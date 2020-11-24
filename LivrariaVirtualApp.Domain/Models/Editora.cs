45e8ea5d52c5b43f2c9786699a1f5e7162e802b8
José Rafael Soares Borges <a39041@alunos.ipb.pt>
2020-11-24 09:39:57 +0000

    Add classes Editora, Livro e Entity

---
+++ LivrariaVirtualApp.Domain/Models/Editora.cs
@@ -0,0 +1,13 @@
+﻿
+
+using LivrariaVirtualApp.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaVirtualApp.Domain.Models
+{
+    public class Editora : Entity{
+
+    }
+}
