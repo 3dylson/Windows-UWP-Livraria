6a8f26a9a9665e92278c5aa4d2647559a45fe7cd
Edylson Manuel Gonçalves Frederico <a39221@alunos.ipb.pt>
2020-12-07 21:22:31 +0100

    Add Console

---
+++ LivrariaVirtualApp.Infrastructure/LivrariaVirtualDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LivrariaVirtualApp.Domain.Models;
+using System;
+
+namespace LivrariaVirtualApp.Infrastructure
+{
+    public class LivrariaVirtualDbContext : DbContext
+    {
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Editora> Editoras { get; set; }
+        public DbSet<Livraria> Livrarias { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Parceiro> Parceiros { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+
+        public LivrariaVirtualDbContext()
+        {
+        }
+
+        public LivrariaVirtualDbContext(DbContextOptions<LivrariaVirtualDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Autor>().HasIndex(i => i.Nome).IsUnique();
+            modelBuilder.Entity<Autor>().Property(i => i.Nome)
+                .IsRequired().HasMaxLength(256);
+
+            modelBuilder.Entity<Editora>().HasIndex(i => i.Nome).IsUnique();
+            modelBuilder.Entity<Editora>().Property(i => i.Nome)
+                .IsRequired().HasMaxLength(256);
+        }
+
+    }
+}
+
---
+++ LivrariaVirtualApp.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LivrariaVirtualApp.Domain;
+using LivrariaVirtualApp.Domain.Repositories;
+using LivrariaVirtualApp.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivrariaVirtualApp.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContextOptions<LivrariaVirtualDbContext> Options { get; }
+
+        public IAutorRepository AutorRepository => new AutorRepository(new LivrariaVirtualDbContext(Options));
+
+        public IEditoraRepository EditoraRepository => new EditoraRepository(new LivrariaVirtualDbContext(Options));
+        public ILivrariaRepository LivrariaRepository => new LivrariaRepository(new LivrariaVirtualDbContext(Options));
+        public ILivroRepository LivroRepository => new LivroRepository(new LivrariaVirtualDbContext(Options));
+        public IParceirosRepository ParceirosRepository => new ParceirosRepository(new LivrariaVirtualDbContext(Options));
+        public IUtilizadorRepository UtilizadorRepository => new UtilizadorRepository(new LivrariaVirtualDbContext(Options));
+
+        public UnitOfWork(DbContextOptions<LivrariaVirtualDbContext> options)
+        {
+            Options = options;
+
+            using (var _dbContext = new LivrariaVirtualDbContext(options))
+            {
+                _dbContext.Database.Migrate();
+            }
+        }
+
+    }
+}
+
