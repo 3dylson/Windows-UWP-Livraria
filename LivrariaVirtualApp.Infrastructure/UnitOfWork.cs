781690aad490717344f15f09e150e51ca7ec649e
Edylson Manuel Gonçalves Frederico <a39221@alunos.ipb.pt>
2020-12-07 21:56:53 +0100

    Add Repository for db

--- LivrariaVirtualApp.Infrastructure/UnitOfWork.cs
+++ LivrariaVirtualApp.Infrastructure/UnitOfWork.cs
@@ -12,13 +12,7 @@ namespace LivrariaVirtualApp.Infrastructure
     {
         private DbContextOptions<LivrariaVirtualDbContext> Options { get; }
 
-        public IAutorRepository AutorRepository => new AutorRepository(new LivrariaVirtualDbContext(Options));
-
-        public IEditoraRepository EditoraRepository => new EditoraRepository(new LivrariaVirtualDbContext(Options));
-        public ILivrariaRepository LivrariaRepository => new LivrariaRepository(new LivrariaVirtualDbContext(Options));
-        public ILivroRepository LivroRepository => new LivroRepository(new LivrariaVirtualDbContext(Options));
-        public IParceirosRepository ParceirosRepository => new ParceirosRepository(new LivrariaVirtualDbContext(Options));
-        public IUtilizadorRepository UtilizadorRepository => new UtilizadorRepository(new LivrariaVirtualDbContext(Options));
+        
 
         public UnitOfWork(DbContextOptions<LivrariaVirtualDbContext> options)
         {
