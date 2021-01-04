47d8cf865f0b399eb55feeb45f6b6321c67d0a72
Edylson Manuel Gonçalves Frederico <a39221@alunos.ipb.pt>
2021-01-04 22:03:45 +0000

    Fix #3

--- LivrariaVirtualApp.Domain/Models/Cart.cs
+++ LivrariaVirtualApp.Domain/Models/Cart.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace LivrariaVirtualApp.Domain.Models
 {
     /// <summary>
-    /// Represents a cart (book + quantity + Subtotal ) on an order.
+    /// Represents a cart on an order.
     /// </summary>
     public class Cart : Entity
     {
@@ -16,36 +16,27 @@ namespace LivrariaVirtualApp.Domain.Models
         /// Gets or sets the quantity of books. 
         /// </summary>
         public int Quantity { get; set; } = 1;
-
-        public decimal Subtotal { get; set; } /* public decimal Subtotal => Cart.Sum(cart => Book.Price * Quantity); */
-
-        public int User_id { get; set; }
-        [ForeignKey("User_id")]
+        
+        
         public int Book_id { get; set; }
-        [ForeignKey("Book_id")]
+        public int User_id { get; set; }
+        public int Order_id { get; set; }
 
+        public Book Book { get; set; }
         public User User { get; set; }
-        public Order Order { get; set; } = new Order();
+        public Order Order { get; set; } /*= new Order();*/
                
         public List<Book> Books { get; set; }
         public Cart() { }
-        public Cart(int quantity, decimal subtotal, int user_id, int cart_id )
+        public Cart(int quantity, int user_id, int book_id, int order_id)
         {
-            this.Quantity = quantity;
-            this.Subtotal = subtotal;
-            this.User_id = user_id;
-            this.Id = cart_id;
-                        
+            Quantity = quantity;
+            Book_id = book_id;
+            User_id = user_id;
+            Order_id = order_id;                      
         }
 
-        public override string ToString()
-        {
-            return $"Quantity:{Quantity}, Subtotal:{Subtotal}€";
-        }
-
-
-
-
+        
     }
 
 
