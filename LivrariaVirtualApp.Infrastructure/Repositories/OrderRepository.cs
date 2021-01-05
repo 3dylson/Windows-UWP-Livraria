aaf6ff2203dcb6c8e601da024a286cb4ec499cdd
Luis Ribeiro <a39215@alunos.ipb.pt>
2021-01-05 20:51:21 +0000

    OrderRepository #2

--- LivrariaVirtualApp.Infrastructure/Repositories/OrderRepository.cs
+++ LivrariaVirtualApp.Infrastructure/Repositories/OrderRepository.cs
@@ -16,50 +16,94 @@ namespace LivrariaVirtualApp.Infrastructure.Repositories
         }
 
 
+        public Task<List<Order>> FindAll()
+        {
+            return _dbContext.Orders.AsNoTracking.ToListAsync();
+        }
+
+        public async Task<List<Order>> FindAllByUserIdAsync(int user_id)
+        {
+            return await _dbContext.Orders.SingleOrDefaultAsync(e => e.User_id == user_id);
+        }
+
+
+        public override async Task<Order> UpsertAsync(Order e)
+        {
+            Order f = null;
+            Order existing = await FindByIdAsync(e.User_id);
+
+            if (existing == null)
+            {
+                if (e.Id == 0)
+                {
+                    f = await CreateAsync(e);
+                }
+                else
+                {
+                    f = await UpdateAsync(e);
+                }
+            }
+            else if (existing.Id == e.Id)
+            {
+                _dbContext.Entry(existing).State = EntityState.Detached;
+                f = await UpdateAsync(e);
+            }
+            await _dbContext.SaveChangesAsync();
+
+            return f;
+        }
+
+        public async Task<Order> DeleteAsync(int order_id)
+        {
+            Order entity = _dbContext.Orders.Remove(order_id).Entity;
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
         public Task<List<Order>> FindAllByStatusStartWithAsync(string status)
         {
-            throw new NotImplementedException();
+            return _dbContext.Orders.Where(c => c.Status.StartsWith(status))
+                .OrderBy(c => c.User_id)
+                .ToListAsync();
         }
 
         public Task<List<Order>> FindAllByUserIdAsync(int user_id)
         {
-            throw new NotImplementedException();
+            return _dbContext.Orders.Where(c => c.User_id.StartsWith(user_id))
+                .OrderBy(c => c.User_id)
+                .ToListAsync();
         }
 
-        public override Task<Order> FindOrCreate(Order e)
+        Task<IEnumerable<Order>> GetForUserAsync(int order_id)
         {
             throw new NotImplementedException();
         }
 
-
-        public override Task<Order> UpsertAsync(Order e)
+        public override async Task<Order> UpsertAsync(Order order)
         {
-            throw new NotImplementedException();
-        }
+            Order f = null;
+            Order existing = await FindAllByUserIdAsync(f.User_id);
 
-        Task IOrderRepository.DeleteAsync(int order_id)
-        {
-            throw new NotImplementedException();
-        }
+            if (existing == null)
+            {
+                if (order.Id == 0)
+                {
+                    order = await CreateAsync(order);
+                }
+                else
+                {
+                    f = await UpdateAsync(order);
+                }
+            }
+            else if (existing.Id == order.Id)
+            {
+                _dbContext.Entry(existing).State = EntityState.Detached;
+                f = await UpdateAsync(order);
+            }
+            await _dbContext.SaveChangesAsync();
 
-        Task<List<Order>> IOrderRepository.FindAllByStatusStartWithAsync(string status)
-        {
-            throw new NotImplementedException();
+            return f;
         }
-
-        Task<List<Order>> IOrderRepository.FindAllByUserIdAsync(int user_id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<IEnumerable<Order>> IOrderRepository.GetForUserAsync(int order_id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<Order> IOrderRepository.UpsertAsync(Order order)
-        {
-            throw new NotImplementedException();
-        }
+    }
     }
 }
