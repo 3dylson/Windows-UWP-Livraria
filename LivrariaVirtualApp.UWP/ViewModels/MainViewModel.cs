0601b3ace83f2ad6cbcec78eda67e443f012be4d
Edylson Manuel Gonçalves Frederico <a39221@alunos.ipb.pt>
2021-01-19 11:16:48 +0000

    Add MainViewModel

---
+++ LivrariaVirtualApp.UWP/ViewModels/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaVirtualApp.UWP.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel() => Task.Run(GetUserListAsync);
+
+        public ObservableCollection<UserViewModel> Users { get; }
+            = new ObservableCollection<UserViewModel>();
+
+        private UserViewModel _selectedUser;
+
+        public UserViewModel SelectedUser
+        {
+            get => _selectedUser;
+            set => Set(ref _selectedUser, value);
+        }
+
+        private bool _isLoading = false;
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => Set(ref _isLoading, value);
+        }
+
+
+        /// <summary>
+        /// Gets the complete list of users from the database.
+        /// </summary>
+        public async Task GetUserListAsync()
+        {
+            await DispatcherHelper.ExecuteOnUIThreadAsync(() => IsLoading = true);
+
+            var customers = await App.UnitOfWork.UserRepository.FindAllAsync();
+            if (customers == null)
+            {
+                return;
+            }
+
+            await DispatcherHelper.ExecuteOnUIThreadAsync(() =>
+            {
+                Users.Clear();
+                foreach (var c in customers)
+                {
+                    Users.Add(new UserViewModel(c));
+                }
+                IsLoading = false;
+            });
+        }
+
+        /// <summary>
+        /// Saves any modified customers and reloads the customer list from the database.
+        /// </summary>
+        public void Sync()
+        {
+            Task.Run(async () =>
+            {
+                IsLoading = true;
+                foreach (var modifiedUser in Users
+                    .Where(user => user.IsModified).Select(user => user.User))
+                {
+                    await App.UnitOfWork.UserRepository.UpsertAsync(modifiedUser);
+                }
+
+                await GetUserListAsync();
+                IsLoading = false;
+            });
+        }
+
+
+    }
+}
--- LivrariaVirtualApp.UWP/ViewModels/UserViewModel.cs
+++ LivrariaVirtualApp.UWP/ViewModels/UserViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using LivrariaVirtualApp.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,38 +10,144 @@ using System.Threading.Tasks;
 
 namespace LivrariaVirtualApp.UWP.ViewModels
 {
-   public class UserViewModel : INotifyPropertyChanged
+   public class UserViewModel : BindableBase, IEditableObject
     {
 
-        public UserViewModel()
+        public UserViewModel(User user = null) => User = user ?? new User();
+
+        private User _user;
+
+        public User User
         {
-            User = new User();
+            get => _user;
+            set
+            {
+                if (_user != value)
+                {
+                    _user = value;
+                    RefreshOrders();
+
+                    // Raise the PropertyChanged event for all properties.
+                    OnPropertyChanged(string.Empty);
+                }
+            }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        public string Name
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            get => User.Name;
+            set
+            {
+                if (value != User.Name)
+                {
+                    User.Name = value;
+                    IsModified = true;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(UserName));
+                }
+            }
         }
 
-        private List<User> _users;
+        public string UserName => IsNewUser && string.IsNullOrEmpty(Name) ? "New user" : $"{Name}";
 
-        public List<User> Users 
+        public string Email
         {
-            get => _users;
-            set => _users = value;
-
+            get => User.Email;
+            set
+            {
+                if (value != User.Email)
+                {
+                    User.Email = value;
+                    IsModified = true;
+                    OnPropertyChanged();
+                }
+            }
         }
 
-        public async void LoadAllUsersAsync()
+        public string Phone
         {
-            Users = await App.UnitOfWork.UserRepository.FindAllAsync();
-
+            get => User.Phone;
+            set
+            {
+                if (value != User.Phone)
+                {
+                    User.Email = value;
+                    IsModified = true;
+                    OnPropertyChanged();
+                }
+            }
         }
 
+        public string Address
+        {
+            get => User.Address;
+            set
+            {
+                if (value != User.Address)
+                {
+                    User.Address = value;
+                    IsModified = true;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public User User { get; set; }
+        /// <summary>
+        /// Gets or sets a value that indicates whether the underlying model has been modified. 
+        /// </summary>
+        /// <remarks>
+        /// Used when sync'ing with the server to reduce load and only upload the models that have changed.
+        /// </remarks>
+        public bool IsModified { get; set; }
+
+        /// <summary>
+        /// Gets the collection of the user's orders.
+        /// </summary>
+        public ObservableCollection<Order> Orders { get; } = new ObservableCollection<Order>();
+
+        private Order _selectedOrder;
+
+        /// <summary>
+        /// Gets or sets the currently selected order.
+        /// </summary>
+        public Order SelectedOrder
+        {
+            get => _selectedOrder;
+            set => Set(ref _selectedOrder, value);
+        }
+
+        private bool _isLoading;
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether to show a progress bar. 
+        /// </summary>
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => Set(ref _isLoading, value);
+        }
+
+        private bool _isNewUser;
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether this is a new user.
+        /// </summary>
+        public bool IsNewUser
+        {
+            get => _isNewUser;
+            set => Set(ref _isNewUser, value);
+        }
+
+        private bool _isInEdit = false;
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether the user data is being edited.
+        /// </summary>
+        public bool IsInEdit
+        {
+            get => _isInEdit;
+            set => Set(ref _isInEdit, value);
+        }
 
         private User _loggedUser;
 
@@ -70,6 +177,23 @@ namespace LivrariaVirtualApp.UWP.ViewModels
         }
 
         public bool IsAdmin => LoggedUser != null && LoggedUser.Admin == 1;
+
+
+        /// <summary>
+        /// Saves user data that has been edited.
+        /// </summary>
+        public async Task SaveAsync()
+        {
+            IsInEdit = false;
+            IsModified = false;
+            if (IsNewUser)
+            {
+                IsNewUser = false;
+                App.UnitOfWork.UserRepository.Add(this);
+            }
+
+            await App.Repository.Customers.UpsertAsync(Model);
+        }
 
 
         internal async Task<bool> DoLoginAsync()
