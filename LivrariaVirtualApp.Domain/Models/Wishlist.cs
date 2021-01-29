c769c71597cc40f888e1547240454dc51364a101
Luis Ribeiro <a39215@alunos.ipb.pt>
2021-01-29 01:49:33 +0000

    Atualização do wishlist.model

--- LivrariaVirtualApp.Domain/Models/Wishlist.cs
+++ LivrariaVirtualApp.Domain/Models/Wishlist.cs
@@ -5,6 +5,7 @@ namespace LivrariaVirtualApp.Domain.Models
     public class Wishlist : Entity
     {
         public string Name { get; set; }
+        public string Color { get; set; }
 
         public int User_id { get; set; }
 
@@ -17,10 +18,11 @@ namespace LivrariaVirtualApp.Domain.Models
         {
         }
 
-        public Wishlist(string name_wishlist, int wishlist_id)
+        public Wishlist(string name_wishlist, int wishlist_id, string colorCode)
         {
             Name = name_wishlist;
             Id = wishlist_id;
+            Color = colorCode;
         }
 
 
